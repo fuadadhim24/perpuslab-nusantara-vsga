1785721d51322a0f5984016eb0a7fe45b03f1fa4
fuadadhim24 <fuadadhim24@gmail.com>
2024-07-17 11:58:31 +0700

    feat: refactor swiper img landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,28 +142,28 @@
           </script>
                     <div class="swiper-wrapper align-items-center">
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-1.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/upi.png" class="img-fluid"
                                 alt=""></div>
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-2.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/um.jpg" class="img-fluid"
                                 alt=""></div>
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-3.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/unesa.jpg" class="img-fluid"
                                 alt=""></div>
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-4.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/polije.png" class="img-fluid"
                                 alt=""></div>
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-5.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/kominfo-removebg.png" class="img-fluid" style="width: 280px; height: auto;"
                                 alt=""></div>
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-6.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/kabupaten_jember-removebg.png" class="img-fluid" style="width: 280px; height: auto;"
                                 alt=""></div>
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-7.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/kampus merdeka.jpg" class="img-fluid"
                                 alt=""></div>
                         <div class="swiper-slide"><img
-                                src="{{ asset('assets/landing_page') }}/img/clients/client-8.png" class="img-fluid"
+                                src="{{ asset('assets/landing_page') }}/img/clients/digitalent.jpg" class="img-fluid"
                                 alt=""></div>
                     </div>
                     <div class="swiper-pagination"></div>
